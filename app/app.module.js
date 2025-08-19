9b94381b154d8954ee930ddc13da85b66ccb973f
Ashutosh <astiwari@amplify.com>
2025-08-19 11:13:31 +0530

    Accessibility fixes for A11Y-2

--- app/app.module.js
+++ app/app.module.js
@@ -1,1 +1,12 @@
-angular.module('todoApp', ['ngRoute']);
+angular.module('todoApp', ['ngRoute']).directive('myDirective', function() {
+	return {
+		restrict: 'A',
+		link: function(scope, element, attrs) {
+			element.on('click keypress', function(event) {
+				if (event.type === 'click' || event.which === 13) {
+					// Handle click or Enter key press
+				}
+			});
+		}
+	};
+});
