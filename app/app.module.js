96002ebbb452983c22751931c91f179e93db4f98
Ashutosh <ashutosh4u1@gmail.com>
2025-08-25 22:11:40 +0530

    Accessibility fixes for A11Y-6

--- app/app.module.js
+++ app/app.module.js
@@ -1,1 +1,20 @@
 angular.module('todoApp', ['ngRoute']);
+
+$scope.highlight = function(index) {
+  drawHighlight(index);
+};
+
+$scope.keyPress = function(event, index) {
+  if (event.key === 'Enter' || event.key === ' ') {
+    selectItem(index);
+    event.preventDefault();
+  }
+};
+
+function drawHighlight(index) {
+  if (!$scope.items[index]) return;
+  var item = $scope.items[index];
+  ctx.strokeStyle = 'blue';
+  ctx.lineWidth = 3;
+  ctx.strokeRect(item.x, item.y, item.w, item.h);
+}
